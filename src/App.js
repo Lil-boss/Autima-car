7a5cb3d23b0f3f743e20cf7a8f8b906dfdc75996
Lil-boss <rshaon09@gmail.com>
2022-05-22 14:32:01 +0600

    navbar complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+
 function App() {
   return (
     <div>
-
+      <Navbar />
+      <Home />
     </div>
   );
 }
