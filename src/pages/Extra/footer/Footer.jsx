870a641fd39cd2b2de3eed2fb53771b3f3e14b21
Lil-boss <rshaon09@gmail.com>
2022-05-26 00:30:23 +0600

    update readme

--- src/pages/Extra/footer/Footer.jsx
+++ src/pages/Extra/footer/Footer.jsx
@@ -3,13 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className="footer p-10 bg-base-100 text-base-content">
-            <div>
-                <span className="footer-title">Services</span>
-                <a href='/' className="link link-hover">Branding</a>
-                <a href='/' className="link link-hover">Design</a>
-                <a href='/' className="link link-hover">Marketing</a>
-                <a href='/' className="link link-hover">Advertisement</a>
-            </div>
+
             <div>
                 <span className="footer-title">Company</span>
                 <a href='/' className="link link-hover">About us</a>
@@ -24,6 +18,7 @@ const Footer = () => {
                     <a href='/'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
                     <a href='/'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
                 </div>
+                <p>All right reserved by &copy;Autima</p>
             </div>
         </footer>
     );
